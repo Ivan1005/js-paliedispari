742532714a8b8565057934ac1aa12583520ff5f8
Ivan <matteraivam@gmail.com>
2020-07-15 13:41:40 +0200

    esercizio

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var parola = prompt("inserisci una parola");
+// PROCEDIMENTO SENZA FUNZIONE
+// CREO LA VARIABILE VUOTA
+// var parola2 = "";
+// // ATTRAVERSO IL CICLO DECREMENTO E PRENDO DALL' ULTIMA ALLA PRIMA LETTERA DELLA PAROLA
+//
+// for ( var i = parola.length -1; i >= 0 ; i--) {
+//   parola2 += parola[i];
+// }
+// // ADESSO CONTROLLO SE LE DUE PAROLE SONO UGUALI E SE LO SONO VUOL DIRE CHE è PALINDROMA
+//
+//  if ( parola2 === parola) {
+//    document.getElementById("risultato").innerHTML = "La parola è palindroma";
+//  } else {
+//    document.getElementById("risultato").innerHTML = "La parola non è palindroma";
+//  }
+
+
+// ORA CREO LA FUNZIONE
+palindroma(parola);
+
+
+
+function palindroma ( stringa) {
+  stringa2 = "";
+  for ( var i = stringa.length -1; i >= 0 ; i--) {
+    stringa2 += stringa[i];
+  }
+  if ( stringa2 === stringa) {
+    document.getElementById("risultato").innerHTML = "La parola è palindroma";
+  } else {
+    document.getElementById("risultato").innerHTML = "La parola non è palindroma";
+  }
+}
